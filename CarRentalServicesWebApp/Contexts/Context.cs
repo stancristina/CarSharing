5be5b90ab4e7f63251a94e14568fc946d960be93
Stan Cristina Sabina <stancristina46@gmail.com>
2020-03-28 20:41:31 +0200

    Add Context and properties

---
+++ CarRentalServicesWebApp/Contexts/Context.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CarRentalServicesWebApp.Contexts;
+using CarRentalServicesWebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarRentalServicesWebApp.Contexts
+{
+    public class Context : DbContext
+    {
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Shop> Shops { get; set; }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Rental> Rentals { get; set; }
+        public DbSet<Client> Clients { get; set; }
+
+        public static bool isMigration = true;
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (isMigration)
+            {
+                optionsBuilder.UseSqlServer(@"Server=.\;Database = DBCarRentalServicesWebApp;Trusted_Connection=True");
+            }
+        }
+        public Context()
+        {
+
+        }
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+
+        }
+    }
+}
+
--- CarRentalServicesWebApp/Startup.cs
+++ CarRentalServicesWebApp/Startup.cs
@@ -6,29 +6,56 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using CarRentalServicesWebApp.Contexts;
+using CarRentalServicesWebApp.Repositories.CityRepository;
+using CarRentalServicesWebApp.Repositories.ShopRepository;
+using CarRentalServicesWebApp.Repositories.CarRepository;
+using CarRentalServicesWebApp.Repositories.RentalRepository;
+using CarRentalServicesWebApp.Repositories.ClientRepository;
+using CarRentalServicesWebApp.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CarRentalServicesWebApp
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        public Startup(IConfiguration configuration)
         {
+            Configuration = configuration;
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddDbContext<Context>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddTransient<ICityRepository, CityRepository>();
+            services.AddTransient<IShopRepository, ShopRepository>();
+            services.AddTransient<ICarRepository, CarRepository>();
+            services.AddTransient<IRentalRepository, RentalRepository>();
+            services.AddTransient<IClientRepository, ClientRepository>();
+        }
+
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.Run(async (context) =>
+            else
             {
-                await context.Response.WriteAsync("Hello World!");
-            });
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader().AllowCredentials());
+            app.UseMvc();
         }
+
     }
 }
